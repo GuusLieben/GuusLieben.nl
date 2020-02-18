49d2b66252f1f1f3e06443125ef85bc55f6a6621
Guus Lieben <guus@xendox.com>
2020-02-18 19:12:40 +0100

    Dynamic dark/light mode, finish API handle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import * as moment from 'moment';
 import {HttpClient} from '@angular/common/http';
+import {range} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,36 @@ export class AppComponent implements OnInit {
   gitProjects: Array<GitProject> = [];
   skillSets: Array<SkillSet> = [];
   work: Array<Work> = [];
+  contributions: number = 0;
+
+  private themes = {
+    selected: 'dark',
+    dark: {
+      bg: 'hsl(240, 10%, 10%)',
+      'bg-light': 'hsl(240, 9%, 15%)',
+      high: 'hsla(315, 29%, 46%, 0.4)',
+      'high-light': 'hsla(315, 29%, 46%, 0.9)',
+      imp: 'hsla(202, 55%, 79%, 0.4)',
+      'imp-light': 'hsla(204, 29%, 55%, 0.87)',
+      text: 'hsl(0, 0%, 80%)',
+      'text-mid': 'hsl(0, 0%, 71%)',
+      'text-light': 'hsl(30, 5%, 15%)'
+    },
+    light : {
+      bg: 'hsl(0, 0%, 100%)',
+      'bg-light': 'hsl(0, 0%, 97%)',
+      high: 'hsla(54, 100%, 62%, 0.4)',
+      'high-light': 'hsla(54, 100%, 62%, 0.9)',
+      imp: 'hsla(197, 100%, 83%, 0.4)',
+      'imp-light': 'hsla(194, 88%, 37%, 0.87)',
+      text: 'hsl(0, 0%, 25%)',
+      'text-mid': 'hsl(0, 0%, 50%)',
+      'text-light': 'hsl(0, 0%, 97%)'
+    }
+  };
 
   constructor(private http: HttpClient) {
+    this.switchTheme('dark');
   }
 
   ngOnInit(): void {
@@ -57,7 +86,7 @@ export class AppComponent implements OnInit {
         {
           language: 'Python',
           frameworks: ['PySerial (RPi)'],
-          since: 2020
+          since: 1
         }
       ]
     },
@@ -67,7 +96,37 @@ export class AppComponent implements OnInit {
           {
             language: 'Rust',
             frameworks: ['Rocket'],
-            since: 2020
+            since: 1
+          }
+        ]
+      },
+      {
+        title: '*NIX Administration',
+        skills: [
+          {
+            language: 'Bash Scripting',
+            frameworks: ['UNIX/BSD'],
+            since: 2016
+          },
+          {
+            language: 'System Administration',
+            frameworks: ['Debian/Fedora (Based)'],
+            since: 2014
+          }
+        ]
+      },
+      {
+        title: 'Databases',
+        skills: [
+          {
+            language: 'SQL',
+            frameworks: ['Microsoft SQL Server / Analysis Services', 'MySQL'],
+            since: 2018
+          },
+          {
+            language: 'NoSQL',
+            frameworks: ['MongoDB', 'Neo4J', 'Google Firestore'],
+            since: 2019
           }
         ]
       }];
@@ -121,15 +180,37 @@ export class AppComponent implements OnInit {
     }, error => {
       console.error(error);
     });
+
+    for (let i=0; i<10; i++) {
+      this.http.get(`https://api.github.com/users/GuusLieben/events?page=${i}`).subscribe(value => {
+        if (Array.isArray(value)) this.contributions += value.length;
+      });
+    }
   }
 
   join(arr: string[]): string {
     return arr.join(', ');
   }
 
+  since(since: number): string {
+    const years = this.years(since);
+    return years > 1 ? `${years} years` : `${years} year`
+  }
+
   years(since: number) {
+    if (since < moment().year() - 2000) return since;
     const years = moment().year() - since;
     return years < 1 ? '< 1' : years;
+  }
+
+  toggleColor() {
+    if (this.themes.selected === 'dark') this.switchTheme('light')
+    else this.switchTheme('dark')
+  }
+
+  switchTheme(id: string) {
+    Object.keys(this.themes[id]).forEach(reg => document.documentElement.style.setProperty(`--c-${reg}`, this.themes[id][reg]))
+    this.themes.selected = id;
   }
 }
 
